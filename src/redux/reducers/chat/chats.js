4166a930bcddccf9fa1df3a7e8e78ae764c0e7fb
doantrungnghia <trungnghia260114@gmail.com>
2022-02-12 02:00:33 +0700

    init

--- src/redux/reducers/chat/chats.js
+++ src/redux/reducers/chat/chats.js
@@ -7,8 +7,8 @@ const initialState = {
   pinned: [],
   status: "active",
   filteredContacts: [],
-  filteredChats: []
-}
+  filteredChats: [],
+};
 
 const chats = (state = initialState, action) => {
   switch (action.type) {
@@ -18,8 +18,8 @@ const chats = (state = initialState, action) => {
         chats: action.chats,
         contacts: action.contacts,
         contactsGroups: action.contactsGroups,
-        contactsByGroups: getContactsByGroups(state, action)
-      }
+        contactsByGroups: getContactsByGroups(state, action),
+      };
     }
     case "GET_CONTACTS":
       return {
@@ -27,88 +27,94 @@ const chats = (state = initialState, action) => {
         chats: action.chats,
         contacts: action.contacts,
         contactsGroups: action.contactsGroups,
-        contactsByGroups: getContactsByGroups(state, action)
-      }
+        contactsByGroups: getContactsByGroups(state, action),
+      };
     case "GET_CHAT_CONTACTS":
-      return { ...state, chatContacts: action.chats }
+      return { ...state, chatContacts: action.chats };
     case "SEND_MESSAGE":
       let sendMsg,
         newChatContacts,
-        oldChatContactsId = state.chatContacts.map(j => j.uid)
+        oldChatContactsId = state.chatContacts.map((j) => j.uid);
       if (state.chats[action.id]) {
-        let oldState = state.chats[action.id].msg
-        sendMsg = [...oldState, action.msg]
+        let oldState = state.chats[action.id].msg;
+        sendMsg = [...oldState, action.msg];
       } else {
         sendMsg = {
           ...state.chats,
           [action.id]: {
             isPinned: action.isPinned,
-            msg: [action.msg]
-          }
-        }
+            msg: [action.msg],
+          },
+        };
       }
       if (!oldChatContactsId.includes(action.id)) {
-        let extractContactInfo = state.contacts.find(k => k.uid === action.id)
-        newChatContacts = state.chatContacts.concat(extractContactInfo)
+        let extractContactInfo = state.contacts.find(
+          (k) => k.uid === action.id
+        );
+        newChatContacts = state.chatContacts.concat(extractContactInfo);
       } else {
-        newChatContacts = state.chatContacts
+        newChatContacts = state.chatContacts;
       }
-      return { ...state, chats: sendMsg, chatContacts: newChatContacts }
+      return { ...state, chats: sendMsg, chatContacts: newChatContacts };
     case "CHANGE_STATUS":
-      return { ...state, status: action.status }
+      return { ...state, status: action.status };
     case "MARK_AS_SEEN":
-      let marked = state.chats[action.id]
+      let marked = state.chats[action.id];
       marked !== undefined &&
-        marked.msg.forEach(msg => {
-          msg.isSeen = true
-        })
-      return { ...state }
+        marked.msg.forEach((msg) => {
+          msg.isSeen = true;
+        });
+      return { ...state };
     case "SEARCH_CONTACTS":
       if (action.query.length) {
-        let filteredContacts = state.contacts.filter(contact => {
-          return contact.displayName
-            .toLowerCase()
-            .includes(action.query.toLowerCase()) || contact.phone
-            .toLowerCase()
-            .includes(action.query.toLowerCase())
-        })
-        let filteredChats = state.chatContacts.filter(chat => {
-          return chat.displayName
-            .toLowerCase()
-            .includes(action.query.toLowerCase()) || chat.phone
-            .toLowerCase()
-            .includes(action.query.toLowerCase())
-        })
-        return { ...state, filteredContacts, filteredChats }
+        let filteredContacts = state.contacts.filter((contact) => {
+          return (
+            contact.displayName
+              .toLowerCase()
+              .includes(action.query.toLowerCase()) ||
+            contact.phone.toLowerCase().includes(action.query.toLowerCase())
+          );
+        });
+        let filteredChats = state.chatContacts.filter((chat) => {
+          return (
+            chat.displayName
+              .toLowerCase()
+              .includes(action.query.toLowerCase()) ||
+            chat.phone.toLowerCase().includes(action.query.toLowerCase())
+          );
+        });
+        return { ...state, filteredContacts, filteredChats };
       } else {
-        return { ...state }
+        return { ...state };
       }
     case "SET_PINNED":
-      let pinned = state.chats[action.id]
+      let pinned = state.chats[action.id];
       if (pinned) {
-        pinned.isPinned = action.value
-        state.chatContacts.sort((a, b) => b.uid - a.uid)
-        return { ...state }
+        pinned.isPinned = action.value;
+        state.chatContacts.sort((a, b) => b.uid - a.uid);
+        return { ...state };
       } else {
-        return { ...state }
+        return { ...state };
       }
     default:
-      return { ...state }
+      return { ...state };
   }
-}
+};
 
 const getContactsByGroups = (state, action) => {
   let { contactsByGroups } = state;
   let { contacts, contactsGroups } = action;
 
-  return contactsByGroups = contactsGroups.map(cg => {
-    contacts.map(c => {
-      if(cg.uid === c.masterSKUGroupUid) {
+  contactsByGroups = contactsGroups.map((cg) => {
+    contacts.map((c) => {
+      if (cg.uid === c.masterSKUGroupUid) {
         cg.childs.push(c);
       }
-    })
+    });
     return cg;
-  })
-}
+  });
 
-export default chats
+  return contactsByGroups;
+};
+
+export default chats;
