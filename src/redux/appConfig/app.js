6fa97c91d07e7b5b4ae638b30986fdb1adc66edb
doantrungnghia <trungnghia260114@gmail.com>
2022-02-28 16:09:21 +0700

    Use .env to appConfig

--- src/redux/appConfig/app.js
+++ src/redux/appConfig/app.js
@@ -3,26 +3,37 @@ export const appConfig = {
   apiKey: "BEF30287-04C0-444C-A266-629FA30A50C6",
 };
 
+const {
+  REACT_APP_USER_REGISTER_ENDPOINT,
+  REACT_APP_USER_LOGIN_ENDPOINT,
+  REACT_APP_USER_LOGOUT_ENDPOINT,
+  REACT_APP_USER_UPDATE_PASSWORD_ENDPOINT,
+  REACT_APP_USER_GET_PROFILES_ENDPOINT,
+  REACT_APP_USER_UPDATE_PROFILES_ENDPOINT,
+  REACT_APP_USER_INVENTORY_ENDPOINT,
+  REACT_APP_USER_CONVERSATIONS_ENDPOINT,
+} = process.env;
+
 export const apiConfig = {
   rootUrl: "https://api.backendless.com/{0}/{1}/",
   endpoint: {
     auth: {
-      register: "http://localhost:3001/dev/signup",
-      login: "http://localhost:3001/dev/login",
-      logout: "http://localhost:3001/dev/logout",
-      updatePassword: "http://localhost:3001/dev/updatePassword",
+      register: REACT_APP_USER_REGISTER_ENDPOINT,
+      login: REACT_APP_USER_LOGIN_ENDPOINT,
+      logout: REACT_APP_USER_LOGOUT_ENDPOINT,
+      updatePassword: REACT_APP_USER_UPDATE_PASSWORD_ENDPOINT,
     },
     client: {
-      getProfiles: "http://localhost:3001/dev/getProfiles",
-      updateProfiles: "http://localhost:3001/dev/updateProfiles",
+      getProfiles: REACT_APP_USER_GET_PROFILES_ENDPOINT,
+      updateProfiles: REACT_APP_USER_UPDATE_PROFILES_ENDPOINT,
     },
     inventory: {
       getInventory:
-        "https://5jg6n0m1tg.execute-api.us-west-2.amazonaws.com/dev/inventory", //'https://enho23tk41op09s.m.pipedream.net'
+        "https://5jg6n0m1tg.execute-api.us-west-2.amazonaws.com/dev/inventory", //'https://enho23tk41op09s.m.pipedream.net',
+      inventoryByUser: REACT_APP_USER_INVENTORY_ENDPOINT,
     },
     conversations: {
-      getConversations:
-        "https://9qmbuwhd14.execute-api.ap-southeast-1.amazonaws.com/dev/conversations",
+      getConversations: REACT_APP_USER_CONVERSATIONS_ENDPOINT,
     },
   },
 };
