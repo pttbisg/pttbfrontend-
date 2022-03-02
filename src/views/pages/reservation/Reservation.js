9327f8fc73cbb51eb19a623fdd60f828dc100ee4
doantrungnghia <trungnghia260114@gmail.com>
2022-03-02 10:25:07 +0700

    Remove useless

--- src/views/pages/reservation/Reservation.js
+++ src/views/pages/reservation/Reservation.js
@@ -6,13 +6,10 @@ import { RefreshCcw } from "react-feather";
 import "../../../assets/scss/pages/invoice.scss";
 import "../../../assets/scss/plugins/tables/_agGridStyleOverride.scss";
 import Breadcrumbs from "../../../components/@vuexy/breadCrumbs/BreadCrumb";
-import { generateInventoryOfCurrentUserExport } from "../../tables/react-tables/TableData";
 import { inventoryByUser } from "../../../redux/actions/reservation";
 import CustomLoadingCellRenderer from "./CustomLoadingCellRenderer";
 import MasterSKUCustomCellRenderer from "./MasterSKUCustomCellRenderer";
 import ReservationOrder from "./ReservationOrder";
-import XLSX from "xlsx";
-import * as FileSaver from "file-saver";
 import "../../../assets/scss/pages/invoice.scss";
 import ReactTable from "react-table";
 import "react-table/react-table.css";
@@ -25,18 +22,10 @@ class Reservation extends React.Component {
     displayInbounds: false,
     data: [],
     dataExport: [],
-    agData: [],
     isLoading: false,
     paginationPageSize: 20,
     currenPageSize: "",
     getPageSize: "",
-    defaultColDef: {
-      sortable: true,
-      editable: false,
-      resizable: false,
-      suppressMenu: true,
-      autoHeight: true,
-    },
     context: { componentParent: this },
     frameworkComponents: {
       customLoadingCellRenderer: CustomLoadingCellRenderer,
@@ -51,18 +40,6 @@ class Reservation extends React.Component {
       return true;
     },
     fullWidthCellRenderer: "masterSKUCustomCellRenderer",
-    columnDefs: [
-      {
-        headerName: "Name",
-        field: "masterSKU",
-        width: 175,
-        filter: false,
-        checkboxSelection: false,
-        headerCheckboxSelectionFilteredOnly: false,
-        headerCheckboxSelection: false,
-        cellRenderer: "masterSKUCustomCellRenderer",
-      },
-    ],
     isLoading: false,
     modal: false,
     fileName: "",
@@ -75,38 +52,7 @@ class Reservation extends React.Component {
   }
 
   toggleModal = () => {
-    this.renderTableData();
     this.setState({ modal: !this.state.modal });
-  };
-
-  handleExport = () => {
-    this.toggleModal();
-    let table = ReactDOM.findDOMNode(this.tableRef);
-    let bookType = this.state.fileFormat.length
-      ? this.state.fileFormat
-      : "xlsx";
-    let wb = XLSX.utils.table_to_book(table, { sheet: "Sheet JS" });
-    let wbout = XLSX.write(wb, { bookType, bookSST: true, type: "binary" });
-
-    const s2ab = (s) => {
-      var buf = new ArrayBuffer(s.length);
-      var view = new Uint8Array(buf);
-      for (var i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xff;
-      return buf;
-    };
-    let file =
-      this.state.fileFormat.length && this.state.fileFormat.length
-        ? `${this.state.fileName}.${this.state.fileFormat}`
-        : this.state.fileName.length
-        ? `${this.state.fileName}.xlsx`
-        : this.state.fileFormat.length
-        ? `excel-sheet.${this.state.fileFormat}`
-        : "excel-sheet.xlsx";
-
-    return FileSaver.saveAs(
-      new Blob([s2ab(wbout)], { type: "application/octet-stream" }),
-      file
-    );
   };
 
   getInventory = () => {
@@ -115,12 +61,9 @@ class Reservation extends React.Component {
     });
     this.props.inventoryByUser((res) => {
       if (res && res.length > 0) {
-        // const dataExport = generateInventoryOfCurrentUserExport(res);
         this.setState({
           data: res,
-          agData: res,
           dataExport: [],
-          // dataExport: dataExport,
           isLoading: false,
         });
       } else {
@@ -130,16 +73,6 @@ class Reservation extends React.Component {
           isLoading: false,
         });
       }
-    });
-  };
-
-  onGridReady = (params) => {
-    this.gridApi = params.api;
-    this.gridColumnApi = params.columnApi;
-    this.setState({
-      currenPageSize: this.gridApi.paginationGetCurrentPage() + 1,
-      getPageSize: this.gridApi.paginationGetPageSize(),
-      totalPages: this.gridApi.paginationGetTotalPages(),
     });
   };
 
@@ -158,118 +91,14 @@ class Reservation extends React.Component {
     }
   };
 
-  renderTableData = () => {
-    let gridRowNode = this.gridApi.getModel();
-    let rawData = [];
-    let dataExport = [];
-    if (gridRowNode) {
-      let rows = gridRowNode.rowsToDisplay;
-      rows.map((r) => {
-        let item = {
-          masterSKU: "",
-          inventory_details: {},
-          balance_stock_left: 0,
-        };
-
-        item.masterSKU = r.data.masterSKU;
-        item.inventory_details.inbounds = r.data.inventory_details.inbounds
-          .child
-          ? r.data.inventory_details.inbounds.child
-          : [];
-        item.inventory_details.sales = r.data.inventory_details.sales.child
-          ? r.data.inventory_details.sales.child
-          : [];
-        item.inventory_details.other_use = r.data.inventory_details.other_use
-          .child
-          ? r.data.inventory_details.other_use.child
-          : [];
-        item.balance_stock_left = r.data.balance_stock_left;
-        rawData.push(item);
-      });
-      // dataExport = generateInventoryOfCurrentUserExport(rawData);
-      // this.setState({
-      //   dataExport: dataExport,
-      // });
-    }
-  };
-
-  methodFromParent = (height, rowIndex) => {
-    let agParent = document.getElementsByClassName(
-      "ag-full-width-container"
-    )[0];
-    let agRows = document.getElementsByClassName("ag-row");
-    let agCurrentRow = agRows[rowIndex];
-    console.log(document.getElementsByClassName("ag-row"));
-    let agParentH = agParent.offsetHeight;
-    let agRowH = agCurrentRow.offsetHeight;
-    agParent.style.height = `${agParentH - agRowH + height}px`;
-    agCurrentRow.style.height = `${height}px`;
-
-    for (let i = 0; i < agRows.length; i++) {
-      if (i === 0) {
-        agRows[0].style.transform = `translateY(${0}px)`;
-      } else {
-        agRows[i].style.height = `${height}px`;
-        agRows[i].style.transform = `translateY(${height * i}px)`;
-      }
-    }
-    for (let r of agRows) {
-      console.log({
-        h: r.style.height,
-        tr: r.style.transform,
-      });
-    }
-  };
-
-  renderSwitch(key, expanded, param, rowData) {}
-
-  onExpanded = (rowData, key, expanded) => {
-    let { data } = this.state;
-    data = data.map((e) => {
-      if (e.masterSKU === rowData.masterSKU) {
-        e.inventory_details[key].expanded = !expanded;
-      }
-      return {
-        ...e,
-      };
-    });
-
-    this.setState({
-      data: data,
-    });
+  filterAvailableMasterSKU = (array) => {
+    return array.filter((item) => item.quantity > 0);
   };
 
   render() {
-    const {
-      agData,
-      data,
-      columnDefs,
-      defaultColDef,
-      isLoading,
-      dataExport,
-    } = this.state;
+    let { data, isLoading } = this.state;
 
-    console.log(data, "data");
-
-    let renderTableData = data.map((col) => {
-      return (
-        <tr
-          key={`${col.masterSKU}&${
-            col.quantity * Math.random()
-          }&${new Date().getTime()}`}
-        >
-          <td>{col.masterSKU}</td>
-          {/* <td>{col.inboundLocation}</td>
-            <td>{col.inboundDate}</td>
-            <td>{col.inboundQty}</td>
-            <td>{col.saleStoreName}</td>
-            <td>{col.saleQty}</td>
-            <td>{col.otherUseReason}</td>
-            <td>{col.otherUsrQty}</td> */}
-          <td>{col.quantity}</td>
-        </tr>
-      );
-    });
+    data = this.filterAvailableMasterSKU(data);
 
     return (
       <React.Fragment>
@@ -304,17 +133,6 @@ class Reservation extends React.Component {
                         value={this.state.value}
                       />
                     </div>
-                    {/* {!isLoading && (
-                      <div className="export-btn">
-                        <Button.Ripple
-                          color="primary"
-                          onClick={this.toggleModal}
-                          // onClick={() => this.gridApi.exportDataAsCsv()}
-                        >
-                          Export
-                        </Button.Ripple>
-                      </div>
-                    )} */}
                   </div>
                 </div>
 
@@ -322,6 +140,9 @@ class Reservation extends React.Component {
                   {data && data.length > 0 ? (
                     <ReactTable
                       data={data}
+                      ref={(instance) => {
+                        this.tableRef = instance;
+                      }}
                       columns={[
                         {
                           Header: "Master SKU",
@@ -356,48 +177,6 @@ class Reservation extends React.Component {
             </CardBody>
           </Card>
         </Col>
-        {/* <Modal
-          isOpen={this.state.modal}
-          toggle={this.toggleModal}
-          className="modal-dialog-centered"
-        >
-          <ModalHeader toggle={this.toggleModal}>Export To Excel</ModalHeader>
-          <ModalBody>
-            <FormGroup>
-              <Input
-                type="text"
-                value={this.state.fileName}
-                onChange={(e) => this.setState({ fileName: e.target.value })}
-                placeholder="Enter File Name"
-              />
-            </FormGroup>
-            <FormGroup>
-              <CustomInput
-                type="select"
-                id="selectFileFormat"
-                name="customSelect"
-                value={this.state.fileFormat}
-                onChange={(e) => this.setState({ fileFormat: e.target.value })}
-              >
-                <option>xlsx</option>
-                <option>csv</option>
-                <option>txt</option>
-              </CustomInput>
-            </FormGroup>
-          </ModalBody>
-          <ModalFooter>
-            <Button
-              color="primary"
-              disabled={this.state.fileName === ""}
-              onClick={this.handleExport}
-            >
-              Export
-            </Button>
-            <Button color="flat-danger" onClick={this.toggleModal}>
-              Cancel
-            </Button>
-          </ModalFooter>
-        </Modal> */}
       </React.Fragment>
     );
   }
